db80061e6b567a2485fcc2fd376c2b9c5ce7e63e
Александр Бубнов <115075731+GCranK@users.noreply.github.com>
2023-02-20 20:32:10 +0500

    Add files via upload

---
+++ js/lang.js
@@ -0,0 +1,126 @@
+const textTranslations = {
+    "home": {
+        en: 'Home',
+        ru: 'Главная'
+    },
+    "about_me": {
+        en: 'About me',
+        ru: 'Обо мне'
+    },
+    "skills": {
+        en: 'Skills',
+        ru: 'Навыки'
+    },
+    "portfolio": {
+        en: 'Portfolio',
+        ru: 'Портфолио'
+    },
+    "contacts": {
+        en: 'Contacts',
+        ru: 'Контакты'
+    },
+    "name": {
+        en: 'Alexander',
+        ru: 'Александр',
+    },
+    "surname": {
+        en: 'Bubnov',
+        ru: 'Бубнов',
+    },
+    "profession": {
+        en: 'Frontend developer',
+        ru: 'Frontend разработчик'
+    },
+    "location_info": {
+        en: '29 years old, Tashkent',
+        ru: '29 лет, Ташкент'
+    },
+    "about_me_title": {
+        en: 'About me',
+        ru: 'Обо мне'
+    },
+    "about_text1": {
+        en: `Hello, I'm Alexander - Frontend developer from Tashkent.`,
+        ru: 'Привет, я Александр — Frontend разработчик из Ташкента'
+    },
+    "about_text2": {
+        en: `I'm interested in website development and everything related to it.`,
+        ru: 'Я интересуюсь разработкой веб-сайтов и всем, что с этим связано.'
+    },
+    "about_text3": {
+        en: `I'm completed the courses "Frontend developer"`,
+        ru: 'Я окончил курсы "Frontend разработчик"'
+    },
+    "about_text4": {
+        en: `at tehnikum school.`,
+        ru: ' в школе tehnikum.',
+    },
+    "about_text5": {
+        en: 'Ready to implement excellent projects',
+        ru: 'Готов реализовать отличные проекты'
+    },
+    "about_text6": {
+        en: 'with wonderful people.',
+        ru: 'с замечательными людьми.'
+    },
+    "skills_title": {
+        en: 'Skills',
+        ru: 'Навыки'
+    },
+    "skills_text": {
+        en: 'I work in such programs as',
+        ru: 'Я работаю в таких программах как'
+    },
+    "portfolio_title": {
+        en: 'Portfolio',
+        ru: 'Портфолио'
+    },
+    "portfolio_text1": {
+        en: 'landing page - Tehnikum.school',
+        ru: 'Landing страница - Tehnikum.school'
+    },
+    "portfolio_text2": {
+        en: 'Landing page - Bella olanje',
+        ru: 'Landing страница - Bella olanje'
+    },
+    "portfolio_text3": {
+        en: 'Landing Page - Contactor-X',
+        ru: 'Landing страница - Contactor-X'
+    },
+    "footer_contacts": {
+        en: 'Contacts',
+        ru: 'Контакты'
+    },
+    "footer_text1": {
+        en: 'Want to know more or just chat?',
+        ru: 'Хотите узнать больше или просто поболтать?'
+    },
+    "footer_text2": {
+        en: 'You are welcome!',
+        ru: 'Добро пожаловать!'
+    },
+    "message": {
+        en: 'Send message',
+        ru: 'Отправить сообщение'
+    },
+    "title": {
+        en: 'Website - Frontend developer portfolio',
+        ru: 'Сайт - портфолио Frontend разработчика'
+    },
+    "skills_text-dom": {
+        en: `Working with DOM tree in 'Javascript'`,
+        ru: `Работа с DOM деревом в 'Javascript'`
+    },
+    "skills_text-sass": {
+        en: 'Sass preprocessor and regular CSS',
+        ru: 'Препроцессор Sass и обычный CSS'
+    },
+    "skills_text-vscode": {
+        en: 'Visual Studio Code - Code Editor',
+        ru: 'Visual Studio Code — Редактор кода'
+    },
+    "skills_text-bem": {
+        en: 'BEM methodology',
+        ru: 'Методология БЭМ'
+    },
+}
